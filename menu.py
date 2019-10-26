96384358e3f40ca4ccd65517ab3e183a82f2ea0f
madus-dev <57049188+madus-dev@users.noreply.github.com>
2019-10-26 22:42:38 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,11 @@
+print("Madus' Cryptographic Analyzer V0.01")
+
+while True:
+    data = input('')
+    if data == "lib":
+        print(">>z340")
+
+    if data == "z340":
+        import lib.z340.py
+
+     
